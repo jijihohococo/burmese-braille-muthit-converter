58141176929d52b10f28c2171f1298357dcd0464
suzume <johnstevemap@gmail.com>
2021-08-02 06:10:05 +0630

    add testing

--- test/index.php
+++ test/index.php
@@ -5,12 +5,9 @@ require_once  __DIR__.'/../src/BurmeseBrailleMuthit.php';
 use JiJiHoHoCoCo\BurmeseBrailleMuthitConverter\BurmeseBrailleMuthit;
 
 if(BurmeseBrailleMuthit::convertBurmeseToBraille('ပွင့် ဖူး ကြ ချိန် အ မှန် ၊ ဝ ဿန္တ လေ ချို ဖျန်း ။')=="⠏⠜⠌⠄⠂ ⠘⠥⠆ ⠅⠢ ⠨⠔⠊⠝⠄ ⠣ ⠍⠭⠝⠄ ⠒ ⠺ ⠰⠹⠝⠤⠞ ⠱⠇⠨⠔⠊⠑ ⠘⠔⠝⠄⠆ ⠲"){
-	echo "Testing Burmese Braille Muthit Converter with syllable breaks are passed.\n";
+	echo "Testing Burmese Braille Muthit Converter with syllable breaks is passed.\n";
 }
 
-print_r(BurmeseBrailleMuthit::convertBurmeseToBraille('၁၉၂၀'));
-
-
-//echo BurmeseBrailleMuthit::convertBurmeseToBraille('သင်္ကြန်');
-// echo '<br>';
-// echo BurmeseBrailleMuthit::convertBurmeseToBraille('ကွေး');
+if(BurmeseBrailleMuthit::convertBurmeseToBraille('၁၉၂၀ ပြည့် ကျောင်း သား သ ပိတ် ကြီး တွင် ဦး ဖိုး ကျား သည် ထင် ရှား သော ခေါင်း ‌ဆောင် တစ် ဦး ဖြစ် သည် ။')=="⠼⠁⠊⠃⠚ ⠏⠢⠷⠄⠂ ⠱⠅⠔⠁⠌⠄⠆ ⠹⠁⠆ ⠹ ⠏⠊⠞⠄ ⠅⠢⠪⠆ ⠞⠜⠌⠄ ⠰⠑⠪⠆ ⠘⠊⠑⠆ ⠅⠔⠁⠆ ⠹⠷⠄ ⠚⠌⠄ ⠗⠭⠁⠆ ⠱⠹⠁ ⠱⠨⠰⠁⠌⠄⠆  ⠱⠖⠁⠌⠄ ⠞⠎⠄ ⠰⠑⠪⠆ ⠘⠢⠎⠄ ⠹⠷⠄ ⠲"){
+	echo "Testing Burmese Braille Muthit Converter with syllable breaks is passed.\n";
+}
