6ba5cc22906134663c5b915d191980a29e589a97
suzume <johnstevemap@gmail.com>
2021-08-01 06:42:58 +0630

    fixing

--- src/helpers.php
+++ src/helpers.php
@@ -8,16 +8,36 @@ function getAnswer($convertedText,$answer){
 	return strpos($convertedText, ' ')==TRUE ? $answer . ' ' : $answer;
 }
 
-function checkNumbers($convertedTextArray){
-	return isset($$convertedTextArray[$key-1]) &&
-	$$convertedTextArray[$key-1]=='u1040' ||
-	$$convertedTextArray[$key-1]=='u1041' ||
-	$$convertedTextArray[$key-1]=='u1042' ||
-	$$convertedTextArray[$key-1]=='u1043' ||
-	$$convertedTextArray[$key-1]=='u1044' ||
-	$$convertedTextArray[$key-1]=='u1045' ||
-	$$convertedTextArray[$key-1]=='u1046' ||
-	$$convertedTextArray[$key-1]=='u1047' ||
-	$$convertedTextArray[$key-1]=='u1048' ||
-	$$convertedTextArray[$key-1]=='u1049';
+function checkNumbers($convertedText){
+	return 
+	$convertedText=='u1040' ||
+	$convertedText=='u1041' ||
+	$convertedText=='u1042' ||
+	$convertedText=='u1043' ||
+	$convertedText=='u1044' ||
+	$convertedText=='u1045' ||
+	$convertedText=='u1046' ||
+	$convertedText=='u1047' ||
+	$convertedText=='u1048' ||
+	$convertedText=='u1049';
 }
+
+function checkForSingleThaWaiTho($convertedText){
+	return
+	$convertedText!=='u103B' &&
+	$convertedText!=='u103C' &&
+	$convertedText!=='u103D' && 
+	$convertedText!=='u103E';
+}
+
+function checkForComplicatedThaWaiTho($convertedText){
+	return
+	$convertedText=='u103B' ||
+	$convertedText=='u103C' ||
+	$convertedText=='u103D' ||
+	$convertedText=='u103E';
+}
+
+// print_r(convertCharacterToCodeArray('ကော်'));
+// print_r(convertCharacterToCodeArray('ကွေး'));
+// print_r(convertCharacterToCodeArray('ကြွေ'));
--- test/index.php
+++ test/index.php
@@ -4,6 +4,7 @@ require_once  __DIR__.'/../src/BurmeseBrailleMuthit.php';
 
 use JiJiHoHoCoCo\BurmeseBrailleMuthitConverter\BurmeseBrailleMuthit;
 
+//print_r(BurmeseBrailleMuthit::convertBurmeseToBraille('ကျေ'));
 
 if(BurmeseBrailleMuthit::convertBurmeseToBraille('ပွင့် ဖူး ကြ ချိန် အ မှန် ၊ ဝ ဿန္တ လေ ချို ဖျန်း ။')=="⠏⠜⠌⠄⠂ ⠘⠥⠆ ⠅⠢ ⠨⠔⠊⠝⠄ ⠣ ⠍⠭⠝⠄ ⠒ ⠺ ⠰⠹⠝⠤⠞ ⠱⠇⠨⠔⠊⠑ ⠘⠔⠝⠄⠆ ⠲"){
 	echo "Testing Burmese Braille Muthit Converter with syllable breaks are passed.\n";
